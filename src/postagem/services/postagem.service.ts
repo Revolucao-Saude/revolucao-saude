17f2b96a05447a0c8491ec8023a64a203c6c9144
João Marcos <barretojm@outlook.com>
2022-10-14 10:41:57 -0300

    PostagemService - Método Deletar

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,6 +1,6 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { ILike, Repository } from "typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
 import {Postagem} from "../entities/postagem.entity";
 
 @Injectable()
@@ -34,7 +34,36 @@ export class PostagemServices {
         })
     } 
 
+    async create (postagem: Postagem): Promise<Postagem>{;
+        return await this.postagemRepository.save(postagem);
+    }
+
+    async update (postagem: Postagem): Promise<Postagem>{
+
+        let buscaPostagem: Postagem = await this.findById(postagem.id);
+
+        if (!buscaPostagem || !postagem.id)
+            throw new HttpException('Postagem não encontrada!' , HttpStatus.NOT_FOUND);
+        
+        return await this.postagemRepository.save(postagem)
+    }   
+
+    async delete (id : number): Promise<DeleteResult> {
+
+        let buscaPostagem = await this.findById(id);
+
+        if (!buscaPostagem)
+            throw new HttpException ('Postagem não encontrada!' , HttpStatus.NOT_FOUND);
+
+        return await this.postagemRepository.delete(id);
+    }
+
+
+
+
+
 
 }
+
 
     
