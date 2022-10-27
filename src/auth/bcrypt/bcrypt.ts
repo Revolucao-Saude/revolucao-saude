ea1243cd0dd2e0c0c80d059033dcfeff6880d503
MarcosDaSilvaGomes <marcossgoficial@hotmail.com.br>
2022-10-27 09:42:26 -0300

    modulo criptografia

---
+++ src/auth/bcrypt/bcrypt.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@nestjs/common";
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class Bcrypt {
+    async criptografarSenha (senha: string): Promise<string>{
+
+        let saltos = 10;
+        return await bcrypt.hash (senha, saltos);
+
+    }
+    async compararSenhas (senhaBanco:string , senhaDigitada:string): Promise <boolean>{
+        return bcrypt.compareSync (senhaDigitada, senhaBanco);
+  }}
