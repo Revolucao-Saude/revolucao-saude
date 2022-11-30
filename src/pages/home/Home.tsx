d7a2117f6c60fcb032e595ce4e8d469de4485415
anaalicefortuna <anaalicefalves@gmail.com>
2022-11-30 12:52:30 -0300

    SignIn

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Home.css";
 import {Grid} from "@material-ui/core";
+import { Login } from "../login/Login";
+
+
 
 function Home(){
     return(
         <>
             TESTE
+            <Login />
+            
         </>
     );
 }
