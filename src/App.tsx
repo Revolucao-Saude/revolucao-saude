7d12bc5ef6fbee55441feff7b826f9294b5d4a00
João Marcos <barretojm@outlook.com>
2022-11-30 10:47:49 -0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
-import { Home } from "./pages/home/Home";
 
 function App() {
-  return (
-    <Home/>
+  return(
+    <Router>
+      <Routes>
+        <Route path='/' /*element={<Home />}*/ />
+        <Route path='/inicio' /*element={<Home />}*/ />
+        <Route path='/perfil' /*element={<Perfil />}*/ />
+        <Route path='/cadastro' /*element={<Cadastro />}*/ />
+        <Route path='/login' /*element={<Login />}*/ />
+        <Route path='/sobre' /*element={<Sobre />}*/ />
+      </Routes>
+    </Router>
   );
 }
 
