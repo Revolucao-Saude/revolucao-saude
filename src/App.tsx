49a43f5e00a91e59eea9acb8dee08b7c5bbae319
Pedro Augusto da Silva Rosa <pedrorosadev20@gmail.com>
2022-11-30 10:09:36 -0300

    init: Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import { Home } from "./pages/home/Home";
+
+function App() {
+  return (
+    <Home/>
+  );
+}
+
+export default App;
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+import {Grid} from "@material-ui/core";
+
+function Home(){
+    return(
+        <>
+            TESTE
+        </>
+    );
+}
+
+export { Home };
