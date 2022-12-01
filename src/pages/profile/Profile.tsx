f02f1571005d664b3e09f8f480721d9b59bd8431
IsmaelMF9469 <ismaelmelo9469@hotmail.com>
2022-12-01 09:49:41 -0300

    correcaoOrtografia

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { AboutMe } from "../../components/aboutMe/AboutMe";
-import { ImageProfile } from "../../components/fotoPerfil/ImageProfile";
+import { ImageProfile } from "../../components/imageProfile/ImageProfile";
 import { PersonalPosts } from "../../components/personalPosts/PersonalPosts";
 
 import "./Profile.css"
