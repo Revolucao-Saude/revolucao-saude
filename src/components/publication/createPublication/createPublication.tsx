7e0f1468641f12c6dc5a6e93c249d290606522a3
Pedro Augusto da Silva Rosa <pedrorosadev20@gmail.com>
2022-12-05 12:51:24 -0300

    style: desenvolvimento componente Criar Publicação

--- src/components/publication/createPublication/createPublication.tsx
+++ src/components/publication/createPublication/createPublication.tsx
@@ -1,56 +1,71 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton, { IconButtonProps } from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Button, Divider, Grid } from "@material-ui/core";
+import PhotoIcon from "@mui/icons-material/Photo";
+import SmartDisplayIcon from "@mui/icons-material/SmartDisplay";
+import EventIcon from "@mui/icons-material/Event";
+import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt';
 
 function CreatePublication() {
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        image="/static/images/cards/paella.jpg"
-        alt="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-      </CardActions>
+      <Grid container spacing={1}>
+        <Grid container item>
+          <Grid item>
+            <CardHeader
+              avatar={
+                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                  R
+                </Avatar>
+              }
+            />
+          </Grid>
+        </Grid>
+
+        <Grid item>
+          <Divider variant="middle" />
+        </Grid>
+
+        <Grid container item>
+          <Grid item>
+            <CardActions disableSpacing>
+              <Button variant="text">
+                <PhotoIcon />
+                Fotos
+              </Button>
+              <Button variant="text">
+                <SmartDisplayIcon />
+                Vídeo
+              </Button>
+              <Button variant="text">
+                <EventIcon />
+                Eventos
+              </Button>
+              <Button variant="text">
+                <AddLocationAltIcon />
+                Localização
+              </Button>
+
+            </CardActions>
+          </Grid>
+        </Grid>
+      </Grid>
     </Card>
   );
 }
 
-export {CreatePublication};
+export { CreatePublication };
