bf19b2384973902fdaed3bfb10f92592fb6a0516
anaalicefortuna <anaalicefalves@gmail.com>
2022-12-08 10:04:27 -0300

    feat: service

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -4,16 +4,36 @@ export const api = axios.create({
     baseURL: 'https://revolucao-saude.onrender.com'
 })
 
-export const cadastroUsuario = async(url: any,dados: any,setDado: any) => {
+export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
     const resposta = await api.post(url,dados)
     setDado(resposta.data)
 }
 
-export const login = async(url: any,dados: any,setDado: any) => {
-    console.log(dados);
-    
+export const login = async(url: any,dados: any,setDado: any) => { 
     const resposta = await api.post(url,dados)
-    console.log(resposta.data.token);
-    
     setDado(resposta.data.token)
+}
+
+export const busca = async(url: any,setDado: any, header: any) => { 
+    const resposta = await api.get(url,header)
+    setDado(resposta.data)
+}
+
+export const buscaId = async(url: any,setDado: any, header: any) => { 
+    const resposta = await api.get(url,header)
+    setDado(resposta.data)
+}
+
+export const post = async(url: any, dados: any, setDado: any, header: any) => { 
+    const resposta = await api.post(url,dados,header)
+    setDado(resposta.data)
+}
+
+export const put = async(url: any, dados: any, setDado: any, header: any) => { 
+    const resposta = await api.put(url,dados,header)
+    setDado(resposta.data)
+}
+
+export const deleteId = async(url: any,header: any) => { 
+    await api.delete(url,header)
 }
