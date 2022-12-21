cfd6f6b7dbac7ded59d1720acb111955339993e1
anaalicefortuna <anaalicefalves@gmail.com>
2022-12-21 16:54:35 -0300

    fix: login e cadastro

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -73,7 +73,7 @@ function Login() {
   return (
     <div className="fotofundo">
       <Grid
-        className={classes.grid}
+        className='grid'
         container
         alignItems="center"
         justifyContent="space-evenly"
@@ -89,7 +89,7 @@ function Login() {
         <Grid
           item
           className={classes.paper}
-          xs={10}
+          xs={9}
           sm={7}
           md={4}
           lg={3}
